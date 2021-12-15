286a9bf68ccc218b2429e9c3fe1da43d03c23076
yangjianbang <yangjianbang@wps.cn>
2021-12-15 17:44:25 +0800

    feat: 读取文件，优化

--- src/app/index.js
+++ src/app/index.js
@@ -1,16 +1,12 @@
 const Koa = require('koa');
 const bodyParser = require('koa-bodyparser');
 const errorHandle = require('./error-handle');
-const userRouter = require('../router/user.router');
-const authRouter = require('../router/auth.router')
+const useRoutes = require('../router/index');
 
 const app = new Koa();
 
 app.use(bodyParser());
-app.use(userRouter.routes());
-app.use(userRouter.allowedMethods());
-app.use(authRouter.routes());
-app.use(authRouter.allowedMethods());
+useRoutes(app);
 
 app.on('error', errorHandle);
 
