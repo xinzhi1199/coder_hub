5fe9b59fa988d740933221d3604512330074ee5d
yangjianbang <yangjianbang@wps.cn>
2021-12-15 15:00:56 +0800

    feat: 用户注册家接口

--- src/main.js
+++ src/main.js
@@ -1,5 +1,6 @@
 const app = require('./app')
 const config = require('./app/config')
+const database = require('./app/database')
 
 app.listen(config.APP_PORT, () => {
     console.log(`服务器在 ${config.APP_HOST}:${config.APP_PORT} 启动成功`);
--- src/service/user.service.js
+++ src/service/user.service.js
@@ -1,8 +1,19 @@
+const connection = require('../app/database');
+
 class UserService {
   async create(user) {
-    // 将user储存到数据库
+    const {name, password} = user;
 
-    return "创建用户成功"
+    // 将user储存到数据库
+    const statement = `INSERT INTO user (name, password) VALUES (?, ?);`;
+
+    const result = await connection.execute(statement, [name, password]);
+
+    console.log(result);
+
+    return result[0];
+
+    // return "创建用户成功"
   }
 }
 
