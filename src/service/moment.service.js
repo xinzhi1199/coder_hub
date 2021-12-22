b922665c6ed1a341e5e1c34f35072e6a19d2fccf
yangjianbang <yangjianbang@wps.cn>
2021-12-22 19:07:15 +0800

    feat: 查询单个动态 - 返回评论数量

--- src/service/moment.service.js
+++ src/service/moment.service.js
@@ -14,7 +14,8 @@ class MomentService {
         const statement = `
           SELECT 
             m.id id, m.content content, m.create_at createTime, m.update_at updateTime,
-            JSON_OBJECT('id', u.id, 'name', u.name) user
+            JSON_OBJECT('id', u.id, 'name', u.name) user,
+            (SELECT COUNT(*) FROM comment c WHERE c.moment_id = m.id) commentCount
           FROM moment m 
           LEFT JOIN user u ON m.user_id = u.id
           WHERE m.id = ?;
