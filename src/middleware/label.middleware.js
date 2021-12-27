3ef58381cfc17983284fa8218ade575faa7ef524
yangjianbang <yangjianbang@wps.cn>
2021-12-27 16:45:46 +0800

    feat: delete log

--- src/middleware/label.middleware.js
+++ src/middleware/label.middleware.js
@@ -4,13 +4,11 @@ const verifyLabelExists = async(ctx, next) => {
     console.log('middleware--verifyLabelExists');
     // 获取参数
     const { labels } = ctx.request.body;
-    console.log('labels---', labels);
 
     // 2.判断每一个标签在label表中是否存在
     const newLabels = [];
     for (const name of labels) {
         const labelResult = await service.getLabelByName(name);
-        console.log('labelResult', labelResult);
         const label = { name };
         if (!labelResult) {
             const result = await service.create(name);
@@ -21,7 +19,6 @@ const verifyLabelExists = async(ctx, next) => {
         }
         newLabels.push(label);
     }
-    console.log('newLabels---', newLabels);
     ctx.labels = newLabels;
     await next();
 }
